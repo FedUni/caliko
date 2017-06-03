fdbd552651ddd6cf5a2f7f1579ecfe0c325d8b93
jsalvo <jmsjr@users.noreply.github.com>
2017-06-03 19:23:46 +1000

    - Javadoc fixes

--- caliko/src/main/java/au/edu/federation/utils/MarshallingUtil.java
+++ caliko/src/main/java/au/edu/federation/utils/MarshallingUtil.java
@@ -25,9 +25,10 @@ public class MarshallingUtil {
 	/**
 	 * Marshalls a FabrikChain as XML to an OutputStream
 	 * 
+	 * @param <T>   The type of FabrikChain
 	 * @param chain The FabrikChain to marshall
 	 * @param os		The OutputStream to write out the XML to
-	 * @throws JAXBException
+	 * @throws JAXBException Any problem marshalling the FabrikChain
 	 */
 	@SuppressWarnings("rawtypes")
 	public static <T extends FabrikChain> void marshall(final T chain, final OutputStream os) throws JAXBException {
@@ -41,9 +42,10 @@ public class MarshallingUtil {
 	/**
 	 * Marshalls a FabrikStructure as XML to an OutputStream
 	 * 
+	 * @param <T>   The type of FabrikStructure
 	 * @param chain The FabrikStructure to marshall
 	 * @param os		The OutputStream to write out the XML to
-	 * @throws JAXBException
+	 * @throws JAXBException Any problem marshalling the FabrikStructure
 	 */	
 	@SuppressWarnings("rawtypes")
 	public static <T extends FabrikStructure> void marshall(final T chain, final OutputStream os) throws JAXBException {
@@ -57,10 +59,11 @@ public class MarshallingUtil {
 	/**
 	 * Unmarshalls a FabrikChain in XML from an InputStream
 	 * 
+	 * @param <T>   The type of FabrikChain
 	 * @param is 		The InputStream of the XML to unmarshall from
 	 * @param clazz	The type of FabrikChain that must be unmarshalled
 	 * @return			The FabrikChain unmarshalled from the InputStream
-	 * @throws JAXBException
+	 * @throws JAXBException Any problem unmarshalling the FabrikChain
 	 */
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public static <T extends FabrikChain> T unmarshallChain(final InputStream is, Class<T> clazz) throws JAXBException {
@@ -75,10 +78,11 @@ public class MarshallingUtil {
 	/**
 	 * Unmarshalls a FabrikStructure in XML from an InputStream
 	 * 
+	 * @param <T>   The type of FabrikStructure	  
 	 * @param is 		The InputStream of the XML to unmarshall from
 	 * @param clazz	The type of FabrikStructure that must be unmarshalled
 	 * @return			The FabrikStructure unmarshalled from the InputStream
-	 * @throws JAXBException
+	 * @throws JAXBException Any problem unmarshalling the FabrikStructure
 	 */	
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public static <T extends FabrikStructure> T unmarshallStructure(final InputStream is, Class<T> clazz) throws JAXBException {
